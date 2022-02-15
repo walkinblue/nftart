1e2763bc6bd66dd2f484d5663c9ba94b0fe4dffc
mark <lixihan@mac.com>
2022-02-15 23:53:10 +0800

    other commit4

--- scripts/vumeter-processor.js
+++ scripts/vumeter-processor.js
@@ -24,22 +24,17 @@ registerProcessor('vumeter', class extends AudioWorkletProcessor {
   process (inputs, outputs, parameters) {
     const input = inputs[0];
 
-    // Note that the input will be down-mixed to mono; however, if no inputs are
-    // connected then zero channels will be passed in.
-    if (input.length > 0) {
+      if (input.length > 0) {
       const samples = input[0];
       let sum = 0;
       let rms = 0;
 
-      // Calculated the squared-sum.
       for (let i = 0; i < samples.length; ++i)
         sum += samples[i] * samples[i];
 
-      // Calculate the RMS level and update the volume.
       rms = Math.sqrt(sum / samples.length);
       this._volume = Math.max(rms, this._volume * SMOOTHING_FACTOR);
 
-      // Update and sync the volume property with the main thread.
       this._nextUpdateFrame -= samples.length;
       if (this._nextUpdateFrame < 0) {
         this._nextUpdateFrame += this.intervalInFrames;
